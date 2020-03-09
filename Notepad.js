c88d17a57a558a84b38201e589297cdee737af9f
Noisey Turtle <bobbyoldani2@gmail.com>
2020-03-08 18:31:17 -0700

    Code works when ran on nodemon

--- Notepad.js
+++ Notepad.js
@@ -1,24 +1,39 @@
 const fs = require("fs");
+const uuidv1 = require("uuidv1");
+const dbJson = require("./db/db.json");
 
 class Notepad {
-    writeToJson(newNote) {
-        console.log(newNote);
-        fs.readFile("./db/db.json", "utf-8", function (err, data) {
-            if (err) throw err;
-            console.log("This is the data response from the fs.readFile: " + data);
-            let jsonData = JSON.parse(data);
-            console.log("This is the jsonData which is parsed: " + jsonData);
-            // console.log(jsonData);
-            jsonData.push(newNote);
-            console.log("This is jsonData after append: " + jsonData);
-            let dataString = JSON.stringify(jsonData);
-            console.log("This is the stringified dataString" + dataString);
-            fs.writeFileSync("db/db.json", dataString); 
-        })
-        
-
+  writeToJson(newNote) {
+    newNote.id = uuidv1();
+    console.log(newNote);
+    fs.readFile("./db/db.json", "utf-8", function(err, data) {
+      if (err) throw err;
+      console.log("This is the data response from the fs.readFile: " + data);
+      let jsonData = JSON.parse(data);
+      // console.log("This is the jsonData which is parsed: " + jsonData);
+      jsonData.push(newNote);
+      //   console.log("This is jsonData after append: " + jsonData);
+      let dataString = JSON.stringify(jsonData);
+      //   console.log("This is the stringified dataString" + dataString);
+      fs.writeFileSync("db/db.json", dataString);
+      console.log("New note has been added to the database");
+    });
+  }
+  deleteNote(deleteId) {
+    console.log("deleteNote dbJson" + dbJson);
+    for (let i = 0; i < dbJson.length; i++) {
+      if (dbJson[i].id === deleteId) {
+        console.log(dbJson[i].id);
+        dbJson.splice(i, 1);
+      }
     }
-
+    let stringDbJson = JSON.stringify(dbJson);
+    console.log("spliced stringified dbJson: " + stringDbJson);
+    fs.writeFileSync("db/db.json", stringDbJson, function(err, results) {
+      if (err) throw err;
+      console.log(results);
+    });
+  }
 }
 
 module.exports = new Notepad();
--- server.js
+++ server.js
@@ -1,42 +1,39 @@
 const express = require("express");
 const path = require("path");
-// const fs = require("fs");
+const fs = require("fs");
 const notepad = require("./Notepad");
 
-// let pad = new Notepad();
 
 let app = express();
 let PORT = process.env.PORT || 3333;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// app.get("/api/notes", function (req, res) {
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
 
-// }) 
+app.post("/api/notes", function(req, res) {
+  // console.log(req.body);
+ res.send(notepad.writeToJson(req.body)) ;
+});
 
-app.post("/api/notes", function (req, res) {
-    // console.log(req.body);
-    notepad.writeToJson(req.body)
+app.delete("/api/notes/:id", function(req, res) {
+    console.log(req.params.id);
+    let deleteId = req.params.id;
+    res.send(notepad.deleteNote(deleteId));
 })
 
-// app.delete("/api/notes/:id", function(req, res) {
-
-// })
-
-
-
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
-
-
